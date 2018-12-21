f242bdc166cc920d4dbd1d797734d6dcf9f10446
Tim Fisher <tgfisher26@gmail.com>
2018-12-21 01:17:41 -0500

    struggling to deploy to pages.. please work.

---
+++ build/precache-manifest.5cc8a906899e63b3a2a8848bcf1e1f7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63fbca3b385800735fa6",
+    "url": "/ClickyMcClick/static/js/runtime~main.63fbca3b.js"
+  },
+  {
+    "revision": "96c26a82e4524c05ffa5",
+    "url": "/ClickyMcClick/static/js/main.96c26a82.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/ClickyMcClick/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "96c26a82e4524c05ffa5",
+    "url": "/ClickyMcClick/static/css/main.08352864.chunk.css"
+  },
+  {
+    "revision": "86b1ac032f74dff31fcc4e068a893093",
+    "url": "/ClickyMcClick/index.html"
+  }
+];
